6826148ff1271c5dea854cd684b7ee09887ed02c
Fazouane Marouane <fazouanem3@gmail.com>
2017-06-09 20:11:52 +0200

    Implementing other directory entry props

--- src/runExample.ts
+++ src/runExample.ts
@@ -15,9 +15,37 @@ console.log('sector size', header.sectorSize)
 console.log('number of fat sectors', header.numberOfFatSectors)
 console.log('number of sectors', sectors.length)
 console.log('number of chains', cfb.fatChain.chains.size)
-cfb.fatChain.chains.forEach((chain, startIndex) =>{
+cfb.fatChain.chains.forEach((chain, startIndex) => {
   console.log(`startIndex ${startIndex}, byteLength ${chain.buffer.byteLength}`)
 })
-cfb.directoryEntries.entries.forEach((entry, index) =>{
-  console.log(`directory entry ${index}. name ${entry.name}`)
+function leftpad (str: string, len: number) {
+  str = String(str);
+  let i = -1;
+  let ch = ' ';
+  len = len - str.length;
+  while (++i < len) {
+    str = ch + str;
+  }
+  return str;
+}
+function rightpad (str: string, len: number) {
+  str = String(str);
+  let i = -1;
+  let ch = ' ';
+  len = len - str.length
+  while (++i < len) {
+    str = str + ch;
+  }
+  return str;
+}
+
+console.log('Directory entries')
+console.log(['index', 'name', 'sector', 'size', 'leftId', 'rightId', 'childId'].join('\t'))
+cfb.directoryEntries.entries.forEach((entry, index) => {
+  console.log([index,
+    rightpad(entry.name, 27),
+    entry.startingSectorLocation, entry.streamSize,
+    leftpad(entry.leftSiblingId.toString(16), 8),
+    leftpad(entry.rightSiblingId.toString(16), 8),
+    leftpad(entry.childId.toString(16), 8)].join('\t'))
 })

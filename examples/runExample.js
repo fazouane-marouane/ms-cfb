1c9ad9b31755af9c850a4e751a547ae11a3593fb
Fazouane Marouane <fazouanem3@gmail.com>
2017-06-11 18:59:27 +0200

    run all the examples

--- examples/runExample.js
+++ examples/runExample.js
@@ -1,60 +1,72 @@
 let readFileSync = require('fs').readFileSync
-let readFromNodeBuffer = require('..').readFromNodeBuffer
+let readFromNodeBuffer = require('../node-build').readFromNodeBuffer
 
-let buffer = readFileSync(process.argv[2])
-let cfb = readFromNodeBuffer(buffer)
-let header = cfb.header
-let sectors = cfb.sectors
-console.log('signature', header.headerSignature)
-console.log('version', header.version)
-console.log('bytesOrder', header.bytesOrder)
-console.log('start of directoryChain', header.startOfDirectoryChain)
-console.log('start of minifat', header.startOfMiniFat)
-console.log('start of difat', header.startOfDifat)
-console.log('sector size', header.sectorSize)
-console.log('number of fat sectors', header.numberOfFatSectors)
-console.log('number of sectors', sectors.length)
-console.log('number of chains', cfb.fatChain.chains.size)
-cfb.fatChain.chains.forEach((chain, startIndex) => {
-  console.log(`startIndex ${startIndex}, byteLength ${chain.buffer.byteLength}`)
-})
+for (let index = 2; index < process.argv.length; index++) {
+  let filename = process.argv[index]
+  console.log('file', filename)
+  processFile(filename)
+  console.log('--------\n\n')
+}
 
-function leftpad (str, len) {
-  str = String(str);
-  let i = -1;
-  let ch = ' ';
-  len = len - str.length;
-  while (++i < len) {
-    str = ch + str;
+function processFile(filename) {
+  let buffer = readFileSync(filename)
+  let cfb = readFromNodeBuffer(buffer)
+  let header = cfb.header
+  let sectors = cfb.sectors
+  console.log('signature', header.headerSignature)
+  console.log('version', header.version)
+  console.log('bytesOrder', header.bytesOrder)
+  console.log('start of directoryChain', header.startOfDirectoryChain)
+  console.log('start of minifat', header.startOfMiniFat)
+  console.log('start of difat', header.startOfDifat)
+  console.log('sector size', header.sectorSize)
+  console.log('number of fat sectors', header.numberOfFatSectors)
+  console.log('number of sectors', sectors.length)
+  console.log('number of chains', cfb.fatChain.chains.size)
+  cfb.fatChain.chains.forEach((chain, startIndex) => {
+    console.log(`startIndex ${startIndex}, byteLength ${chain.buffer.byteLength}`)
+  })
+
+  function leftpad(str, len) {
+    str = String(str);
+    let i = -1;
+    let ch = ' ';
+    len = len - str.length;
+    while (++i < len) {
+      str = ch + str;
+    }
+    return str;
   }
-  return str;
-}
-function rightpad (str, len) {
-  str = String(str);
-  let i = -1;
-  let ch = ' ';
-  len = len - str.length
-  while (++i < len) {
-    str = str + ch;
+
+  function rightpad(str, len) {
+    str = String(str);
+    let i = -1;
+    let ch = ' ';
+    len = len - str.length
+    while (++i < len) {
+      str = str + ch;
+    }
+    return str;
   }
-  return str;
+
+  function ignoreSpecialValues(value) {
+    return value <= 0xFFFFFFFA ? value.toString() : '-'
+  }
+
+  console.log('Directory entries')
+  console.log(['index', rightpad('name', 32), 'sector', 'size',
+    rightpad('leftId', 8), rightpad('rightId', 8), rightpad('childId', 8)
+  ].join('\t'))
+  cfb.directoryEntries.entries.forEach((entry, index) => {
+    console.log([index,
+      rightpad(entry.name, 32),
+      ignoreSpecialValues(entry.startingSectorLocation),
+      entry.streamSize,
+      leftpad(ignoreSpecialValues(entry.leftSiblingId), 8),
+      leftpad(ignoreSpecialValues(entry.rightSiblingId), 8),
+      leftpad(ignoreSpecialValues(entry.childId), 8)
+    ].join('\t'))
+  })
+
+  console.log('directories', cfb.root)
 }
-
-function ignoreSpecialValues(value) {
-  return value <= 0xFFFFFFFA? value.toString(): '-'
-}
-
-console.log('Directory entries')
-console.log(['index', rightpad('name', 32), 'sector', 'size',
-  rightpad('leftId', 8), rightpad('rightId', 8), rightpad('childId', 8)].join('\t'))
-cfb.directoryEntries.entries.forEach((entry, index) => {
-  console.log([index,
-    rightpad(entry.name, 32),
-    ignoreSpecialValues(entry.startingSectorLocation),
-    entry.streamSize,
-    leftpad(ignoreSpecialValues(entry.leftSiblingId), 8),
-    leftpad(ignoreSpecialValues(entry.rightSiblingId), 8),
-    leftpad(ignoreSpecialValues(entry.childId), 8)].join('\t'))
-})
-
-console.log('directories', cfb.root)

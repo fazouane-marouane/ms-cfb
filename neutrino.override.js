eb30b00d3e633f7a2b41031d0c1f8122c20ad9a1
Fazouane Marouane <fazouanem3@gmail.com>
2017-06-11 20:13:38 +0200

    fix liveReload

--- neutrino.override.js
+++ neutrino.override.js
@@ -1,4 +1,9 @@
+const { path, pathOr } = require('ramda')
+const { join, dirname } = require('path')
+
 module.exports = neutrino => {
+  const defaultEntry = join(neutrino.options.source, 'index.js')
+  const defaultTsEntry = join(neutrino.options.source, 'index.ts')
   neutrino.options.output = './dist'
   neutrino.config
     .output
@@ -13,4 +18,16 @@ module.exports = neutrino => {
       .delete('chunk')
       .delete('named-modules')
       .end()
+    .when(process.env.NODE_ENV === 'development', config => {
+      const protocol = process.env.HTTPS ? 'https' : 'http'
+      const host = process.env.HOST || pathOr('localhost', ['options', 'config', 'devServer', 'host'], neutrino)
+      const port = process.env.PORT || pathOr(5000, ['options', 'config', 'devServer', 'port'], neutrino)
+      config
+        .entry('index')
+          .clear()
+          .add(neutrino.options.entry === defaultEntry ? defaultTsEntry : neutrino.options.entry)
+          .prepend(`webpack-dev-server/client?${protocol}://${host}:${port}/`)
+          .prepend('webpack/hot/dev-server')
+          .end()
+    })
 }

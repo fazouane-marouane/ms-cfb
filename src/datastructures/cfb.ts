86d14739daa6952095cb3080011860f403ebf526
Fazouane Marouane <fazouanem3@gmail.com>
2017-06-12 06:12:31 +0200

    refactoring of the RedBlack tour

--- src/datastructures/cfb.ts
+++ src/datastructures/cfb.ts
@@ -1,7 +1,7 @@
 import { Header } from './header'
 import { FatChain } from './fatCHain'
-import { DirectoryEntries } from './directoryEntries'
-import { HeaderView, DifatSectorView, FatSectorView } from './dataViews'
+import { getDirectoryEntries, buildHierarchy } from './directoryEntries'
+import { HeaderView, DifatSectorView, FatSectorView, DirectoryEntryView } from './dataViews'
 import { SectorType, ObjectType, StreamType } from './enums'
 import { VirtualDirectory, VirtualFile } from './directory'
 import { chunkBuffer } from '../helpers'
@@ -16,15 +16,17 @@ export class CFB {
     this.buildFatSectors()
     this.buildDirectoryEntries()
     this.buildMiniFatSectors()
-    this.buildDirectoryHierarchy()
+    // build the directory's hierarchy
+    this.root = buildHierarchy(this.directoryEntries, this.header.miniSectorCutoff,
+      this.fatChain.chains, this.miniFatChain.chains)
   }
 
-  public buildSectors() {
+  private buildSectors() {
     let sectorSize = this.header.sectorSize
     this.sectors = chunkBuffer(this.buffer.slice(sectorSize), sectorSize)
   }
 
-  public buildFatSectors() {
+  private buildFatSectors() {
     let visitedSectors = new Set<number>()
     this.fatSectors = this.getDifatArray(visitedSectors)
       .map(sectorNumber => {
@@ -56,78 +58,28 @@ export class CFB {
       .filter(sectorNumber => sectorNumber <= SectorType.MAXREGSECT)
   }
 
-  public buildDirectoryEntries() {
+  private buildDirectoryEntries() {
     let startOfDirectoryChain = this.header.startOfDirectoryChain
     if(startOfDirectoryChain !== SectorType.ENDOFCHAIN) {
       if(!this.fatChain.chains.has(startOfDirectoryChain)) {
         throw new Error(`Directory chain sector not found. It was supposed to be available at ${startOfDirectoryChain}.`)
       }
-      this.directoryEntries = new DirectoryEntries(this.fatChain.chains.get(startOfDirectoryChain)!)
+      this.directoryEntries = getDirectoryEntries(this.fatChain.chains.get(startOfDirectoryChain)!)
     }
   }
 
-  public buildMiniFatSectors() {
+  private buildMiniFatSectors() {
     let startOfMiniFat = this.header.startOfMiniFat
     if(startOfMiniFat !== SectorType.ENDOFCHAIN) {
       if(!this.fatChain.chains.has(startOfMiniFat)) {
         throw new Error(`MiniFAT sector not found. It was supposed to be available at ${startOfMiniFat}.`)
       }
       let miniFatView = this.fatChain.chains.get(startOfMiniFat)!
-      let miniStreamView = this.fatChain.chains.get(this.directoryEntries.entries[0].startingSectorLocation)!
+      let miniStreamView = this.fatChain.chains.get(this.directoryEntries[0].startingSectorLocation)!
       let sectorSize = this.header.miniSectorSize
       let miniStreamSectors = chunkBuffer(miniStreamView, sectorSize)
       this.miniFatChain = new FatChain([new FatSectorView(miniFatView)], miniStreamSectors)
     }
-  }
-
-  public buildDirectoryHierarchy() {
-    let directories = new Map<number, VirtualDirectory>()
-    let children = new Map<number, number[]>()
-    this.directoryEntries.entries.forEach((entry, index) => {
-      if (entry.objectType !== ObjectType.STREAM) {
-        directories.set(index, new VirtualDirectory)
-        let currentChildren: number[] = []
-        let toExplore: number[] = [entry.childId]
-        while (toExplore.length > 0) {
-          let currentIndex = toExplore.pop()!
-          currentChildren.push(currentIndex)
-          let currentEntry = this.directoryEntries.entries[currentIndex]
-          if (currentEntry.leftSiblingId !== StreamType.NOSTREAM) {
-            toExplore.push(currentEntry.leftSiblingId)
-          }
-          if (currentEntry.rightSiblingId !== StreamType.NOSTREAM) {
-            toExplore.push(currentEntry.rightSiblingId)
-          }
-        }
-        children.set(index, currentChildren)
-      }
-    })
-    let toExplore = [0]
-    while (toExplore.length > 0) {
-      let currentDirectoryIndex = toExplore.pop()!
-      let currentDirectory = directories.get(currentDirectoryIndex)!
-      for(let childId of children.get(currentDirectoryIndex)!) {
-        let child = this.directoryEntries.entries[childId]
-        if (child.objectType !== ObjectType.STREAM) {
-          currentDirectory.subdirectories.set(child.name, directories.get(childId)!)
-          toExplore.push(childId)
-        }
-        else {
-          let chains = this.fatChain.chains
-          if(child.streamSize < this.header.miniSectorCutoff) {
-            chains = this.miniFatChain.chains
-          }
-          let sectorId = child.startingSectorLocation
-          if(sectorId <= SectorType.MAXREGSECT) {
-            currentDirectory.files.set(child.name, new VirtualFile(chains.get(sectorId)!.slice(0, child.streamSize)))
-          }
-          else {
-            currentDirectory.files.set(child.name, new VirtualFile(new Uint8Array(0).buffer))
-          }
-        }
-      }
-    }
-    this.root = directories.get(0)!
   }
 
   public header: Header
@@ -140,7 +92,7 @@ export class CFB {
 
   public miniFatChain: FatChain
 
-  public directoryEntries: DirectoryEntries
+  public directoryEntries: DirectoryEntryView[]
 
   public root: VirtualDirectory
 }
--- src/datastructures/directoryEntries.ts
+++ src/datastructures/directoryEntries.ts
@@ -1,13 +1,74 @@
 import { DirectoryEntryView } from './dataViews'
-import { ObjectType } from './enums'
+import { ObjectType, StreamType, SectorType } from './enums'
 import { chunkBuffer } from '../helpers'
+import { VirtualDirectory, VirtualFile } from './directory'
 
-export class DirectoryEntries {
-  constructor(buffer: ArrayBuffer) {
-    this.entries = chunkBuffer(buffer, 128)
-      .map(chunk => new DirectoryEntryView(chunk))
-      .filter(entry => entry.objectType !== ObjectType.UNALLOCATED)
+export function getDirectoryEntries(buffer: ArrayBuffer): DirectoryEntryView[] {
+  let entries = chunkBuffer(buffer, 128)
+    .map(chunk => new DirectoryEntryView(chunk))
+    .filter(entry => entry.objectType !== ObjectType.UNALLOCATED)
+  return entries
+}
+
+function redBlackTreeTour(rootId: number, entries: DirectoryEntryView[],
+  foundFile: (entry: DirectoryEntryView) => void,
+  foundDirectory: (entry: DirectoryEntryView, id: number) => void): void {
+  let toExplore: number[] = [rootId]
+  while (toExplore.length > 0) {
+    let currentIndex = toExplore.pop()!
+    let currentEntry = entries[currentIndex]
+    if (currentEntry.objectType === ObjectType.STREAM) {
+      foundFile(currentEntry)
+    }
+    else {
+      foundDirectory(currentEntry, currentIndex)
+    }
+    if (currentEntry.leftSiblingId !== StreamType.NOSTREAM) {
+      toExplore.push(currentEntry.leftSiblingId)
+    }
+    if (currentEntry.rightSiblingId !== StreamType.NOSTREAM) {
+      toExplore.push(currentEntry.rightSiblingId)
+    }
   }
+}
 
-  public entries: DirectoryEntryView[]
+function completeTreeTour(rootId: number, entries: DirectoryEntryView[],
+  foundFile: (entry: DirectoryEntryView, parentId: number) => void,
+  foundDirectory: (entry: DirectoryEntryView, currentId: number, parentId: number) => void) {
+  let toExplore = [rootId]
+  while (toExplore.length > 0) {
+    let currentIndex = toExplore.pop()!
+    let firstChild = entries[currentIndex].childId
+    redBlackTreeTour(firstChild, entries, entry => {
+      foundFile(entry, currentIndex)
+    }, (entry, entryId) => {
+      foundDirectory(entry, entryId, currentIndex)
+      if (entry.childId !== StreamType.NOSTREAM) {
+        toExplore.push(entryId)
+      }
+    })
+  }
+}
+
+export function buildHierarchy(entries: DirectoryEntryView[], miniSectorCutoff: number,
+  fatChain: Map<number, ArrayBuffer>, miniFatChain: Map<number, ArrayBuffer>): VirtualDirectory {
+  let directories = new Map<number, VirtualDirectory>()
+  entries.forEach((entry, index) => {
+    if (entry.objectType !== ObjectType.STREAM) {
+      directories.set(index, new VirtualDirectory)
+    }
+  })
+  completeTreeTour(0, entries, (entry, parentId) => {
+    let chains = fatChain
+    if (entry.streamSize < miniSectorCutoff) {
+      chains = miniFatChain
+    }
+    let sectorId = entry.startingSectorLocation
+    directories.get(parentId)!.files.set(entry.name, new VirtualFile(
+      sectorId <= SectorType.MAXREGSECT ? chains.get(sectorId)!.slice(0, entry.streamSize) :
+        new Uint8Array(0).buffer))
+  }, (entry, entryId, parentId) => {
+    directories.get(parentId)!.subdirectories.set(entry.name, directories.get(entryId)!)
+  })
+  return directories.get(0)!
 }

bd63806fb90535b682c0f5edaa8db18918b35660
Fazouane Marouane <fazouanem3@gmail.com>
2017-06-14 23:32:57 +0200

    enum names use a lot of space in the final bundle

--- src/datastructures/enums/index.ts
+++ src/datastructures/enums/index.ts
@@ -1,36 +1,40 @@
 /**
  *
  */
-export enum ColorFlag {
-  RED = 0x00,
-  BLACK = 0x01
+// tslint:disable-next-line:variable-name
+export const ColorFlag = {
+  RED: 0x00,
+  BLACK: 0x01
 }
 
 /**
  *
  */
-export enum ObjectType {
-  UNALLOCATED = 0x00,
-  STORAGE = 0x01,
-  STREAM = 0x02,
-  ROOTSTORAGE = 0x05
+// tslint:disable-next-line:variable-name
+export const ObjectType = {
+  UNALLOCATED: 0x00,
+  STORAGE: 0x01,
+  STREAM: 0x02,
+  ROOTSTORAGE: 0x05
 }
 
 /**
  *
  */
-export enum SectorType {
-  MAXREGSECT = 0xFFFFFFFA,
-  DIFSECT = 0xFFFFFFFC,
-  FATSECT = 0xFFFFFFFD,
-  ENDOFCHAIN = 0xFFFFFFFE,
-  FREESECT = 0xFFFFFFFF
+// tslint:disable-next-line:variable-name
+export const SectorType = {
+  MAXREGSECT: 0xFFFFFFFA,
+  DIFSECT: 0xFFFFFFFC,
+  FATSECT: 0xFFFFFFFD,
+  ENDOFCHAIN: 0xFFFFFFFE,
+  FREESECT: 0xFFFFFFFF
 }
 
 /**
  *
  */
-export enum StreamType {
-  MAXREGSID = 0xFFFFFFFA,
-  NOSTREAM = 0xFFFFFFFF
+// tslint:disable-next-line:variable-name
+export const StreamType = {
+  MAXREGSID: 0xFFFFFFFA,
+  NOSTREAM: 0xFFFFFFFF
 }
